8472cf729f4473ef9ef4bad8549b356f8d0b8e59
Gregory V. Bilello <gregory.bilello@gmail.com>
2017-02-28 14:33:30 -0500

    Add line animations.

---
+++ line.js
@@ -0,0 +1,25 @@
+var angle = 0;
+var rate = 20;
+
+function setup() {
+	createCanvas(720, 400);
+	line2 = width / 2 - 10;
+	line1 = line2 - 20 - 10;
+	line3 = line2 + 20 + 10;
+	noStroke();
+	fill(255);
+}
+
+function draw() {
+	background(0);
+
+	h1 = sin(angle) * rate;
+	h2 = sin(angle + PI / 8) * rate;
+	h3 = sin(angle + PI / 4) * rate;
+
+	rect(line1, height / 2 - 20, 20, h1);
+	rect(line2, height / 2 - 20, 20, h2);
+	rect(line3, height / 2 - 20, 20, h3);
+
+	angle += 0.02;
+}
